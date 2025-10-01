d9a9e0186b80ef1b25d658d866f9d776a6fdf323
Haimonmon <beankor8@gmail.com>
2025-10-01 12:45:15 +0800

    Test: barnes and noble support

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import snippy
 
-# snippy.seek_checkup(headless = False)
+snippy.seek_checkup(headless = False)
 
 snippy.seek_openlibrary(
-    online = True,
+    online = False,
     headless = True,
     total_books = 150,
     total_subject = 200,
     total_tabs = 3
 )
+
+snippy.seek_barnesnobles(
+    online = False,
+    headless = True,
+    total_books = 150,
+    total_subject = 200,
+    total_tabs = 3
+)
+
